3fa78533d23d7ee66afe2bead4427fd71bc9f74b
Thaïs Simões <thay.ssim@gmail.com>
2023-01-11 20:18:52 -0300

    First commit

---
+++ src/components/button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import './style.scss';
+
+class Button extends React.Component{
+    render(){
+        return(
+            <button className='botao'>Botão</button>
+        )
+    }
+}
+
+export default Button;
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from '../components/form';
+import List from '../components/list';
+// import './style.scss';
+
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
